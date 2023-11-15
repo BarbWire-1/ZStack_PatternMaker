3d847a7379f426ca690b5de6b192f7a132022cb4
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-11-15 09:33:16 +0100

    added window.onload to prevent delayed rendering

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import { ZStackGrid, createEventHandlers } from './ZStack/index.js';
 
-// USAGE - INIT
-// the HTMLelements to appent into the components slot as template string
-const elements = `
+window.onload = e => {
+    // USAGE - INIT
+    // the HTMLelements to appent into the components slot as template string
+    const elements = `
     <div class="square one"></div>
     <div class="square two"></div>
     <div class="square three"></div>
@@ -11,29 +12,30 @@ const elements = `
     <div class="square six"></div>
 `;
 
-const zStackGrid = new ZStackGrid({
-	parentId: 'container', // required
-	rows: 6, // optional - default 6
-	columns: 6, // optional - default 6
-	alignment: 'bottom-right', // optional - default 'default' (same as 'center')
-	mirrorType: 'both', // optional - default 'none'
-	content: elements,
-});
+    const zStackGrid = new ZStackGrid({
+        parentId: 'container', // required
+        rows: 6, // optional - default 6
+        columns: 6, // optional - default 6
+        alignment: 'bottom-right', // optional - default 'default' (same as 'center')
+        mirrorType: 'both', // optional - default 'none'
+        content: elements,
+    });
 
 
 
-// -------------------------------------------------------------------------------------TESTING
-// Event Delegation
-const { onClick, onInput } = createEventHandlers(zStackGrid);
+    // -------------------------------------------------------------------------------------TESTING
+    // Event Delegation
+    const { onClick, onInput } = createEventHandlers(zStackGrid);
 
-const controlPanel = document.getElementById('control-panel');
-controlPanel.addEventListener('click', onClick);
-controlPanel.addEventListener('input', onInput);
+    const controlPanel = document.getElementById('control-panel');
+    controlPanel.addEventListener('click', onClick);
+    controlPanel.addEventListener('input', onInput);
 
-// testing to include svg
-// const nonRootSvg = document.querySelectorAll('svg :not(svg)');
-// console.log(nonRootSvg)
-// zStackGrid.alignment = 'bottom-right';// ok
-//zStackGrid.mirrorType = 'both';// ok
-//zStackGrid.content = `<div class="square one"></div>`// ok
-//zStackGrid.addContent(`<div class="square one"></div>`);// ok
+    // testing to include svg
+    // const nonRootSvg = document.querySelectorAll('svg :not(svg)');
+    // console.log(nonRootSvg)
+    // zStackGrid.alignment = 'bottom-right';// ok
+    //zStackGrid.mirrorType = 'both';// ok
+    //zStackGrid.content = `<div class="square one"></div>`// ok
+    //zStackGrid.addContent(`<div class="square one"></div>`);// ok
+}
